81a0ede66afc65e6f8a7e839bd594c008945b966
Storm Joannes <storm.joannes@gmail.com>
2020-04-16 20:54:39 +0200

    Tabel

--- AP/src/Autoverhuur/Main.java
+++ AP/src/Autoverhuur/Main.java
@@ -14,14 +14,14 @@ public class Main {
         Auto a1 = new Auto("Peugeot 207", 50);
         ah1.setGehuurdeAuto(a1);
         ah1.setAantalDagen(4);
-        System.out.println("Eerste autohuur:" + ah1.toString() + "\n");
+        System.out.println("Eerste autohuur:" + ah1.toString() + ah1.totaalPrijs() + "\n");
 
         Autohuur ah2 = new Autohuur();
         Auto a2 = new Auto("Ferrari", 3500);
         ah2.setGehuurdeAuto(a2);
         ah2.setHuurder(k);
         ah2.setAantalDagen(1);
-        System.out.println("Tweede autohuur:" + ah2.toString() + "\n");
+        System.out.println("Tweede autohuur:" + ah2.toString() + ah2.totaalPrijs() +"\n");
 
         System.out.println("Gehuurd: " + ah1.getGehuurdeAuto());
         System.out.println("Gehuurd: " + ah2.getGehuurdeAuto());
